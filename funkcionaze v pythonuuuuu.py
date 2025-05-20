d645a6d259201ff3375dc030514c42c25bd403fb
TomasHavlik01 <tomas.havlik@infis.cz>
2025-05-20 12:10:27 +0200

    Add files via upload

---
+++ funkcionaze v pythonuuuuu.py
@@ -0,0 +1,33 @@
+#blud se nespusti, dokud neni zavolanej
+def pozdrav():
+    print("ahoj")
+
+pozdrav()    
+
+def soucet():
+    a = 5
+    b = 10
+    print(a+b)
+
+soucet()
+
+
+
+def jméno():
+    jmeno = input("Zadejte jméno")
+    print("ahoj", jmeno)
+
+jméno()
+
+def pole():
+     polee = [5,4,3,1,2]
+     print(sum(polee)/len(polee))
+
+
+pole()
+
+def neco():
+    for i in range(5):
+        print("ahoj")
+
+neco()
